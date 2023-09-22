21aea68d21dddc0ebf364c56b9a5bc95ad7aa3cf
m29dev <michalmajchrzak.it@gmail.com>
2023-09-22 19:30:04 +0200

    update

--- server/app.js
+++ server/app.js
@@ -22,7 +22,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 const server = http.createServer(app)
 const io = new Server(server, {
     cors: {
-        origin: 'http://localhost:4000',
+        origin: 'https://socialcloudserver.onrender.com',
         methods: ['GET', 'POST']
     }
 })
